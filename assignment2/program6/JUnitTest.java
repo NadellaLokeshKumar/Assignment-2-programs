93fa4da576cee742ed5b0faf5cba4096c8d938ef
NadellaLokeshKumar <lokeshnadella99@gmail.com>
2020-02-17 20:18:07 +0530

    Assignment-2

--- assignment2/program6/JUnitTest.java
+++ assignment2/program6/JUnitTest.java
@@ -2,9 +2,10 @@ package com.ito.assignment2.program6;
 
 import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 import java.time.Month;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class JUnitTest {
 
@@ -27,12 +28,14 @@ class JUnitTest {
 		assertNull(db3);
 	}
 	
-	@Test(expected = CustomException.class) 
-	 void test3() throws CustomException{
-		DateBand db1 = new DateBand(LocalDate.of(2020, Month.JANUARY, 1), LocalDate.of(2020, Month.JANUARY, 15));
-		DateBand db2 = new DateBand(LocalDate.of(2020, Month.JANUARY, 16), LocalDate.of(2020, Month.FEBRUARY, 20));
-		DateBand db = new DateBand();
-		DateBand db3 = db.findOverlappingBand(db1, db2);
+	@Test
+	public void shouldRaiseAnException() throws CustomException {
+	    Assertions.assertThrows(CustomException.class, () -> {
+	    	DateBand db1 = new DateBand(LocalDate.of(2020, Month.JANUARY, 1), LocalDate.of(2020, Month.JANUARY, 15));
+			DateBand db2 = new DateBand(LocalDate.of(2020, Month.JANUARY, 16), LocalDate.of(2020, Month.FEBRUARY, 20));
+			DateBand db = new DateBand();
+			DateBand db3 = db.findOverlappingBand(db1, db2);
+	    });
 	}
 	
 }
--- assignment2/program6/MainClass.java
+++ assignment2/program6/MainClass.java
@@ -18,7 +18,7 @@ public class MainClass {
 
 	public static void main(String[] args) throws CustomException {
 		DateBand db1 = new DateBand(LocalDate.of(2020, Month.JANUARY, 1), LocalDate.of(2020, Month.JANUARY, 31));
-		DateBand db2 = new DateBand(LocalDate.of(2020, Month.JANUARY, 15), LocalDate.of(2020, Month.FEBRUARY, 30));
+		DateBand db2 = new DateBand(LocalDate.of(2020, Month.JANUARY, 15), LocalDate.of(2020, Month.FEBRUARY, 20));
 		DateBand db = new DateBand();
 		DateBand db4 = db.findOverlappingBand(db1, db2);
 		System.out.println(db4);
