0a69ec9b2c3fb2edfda35736f8d355cf406a55e0
NadellaLokeshKumar <lokeshnadella99@gmail.com>
2020-02-18 12:27:01 +0530

    Assignment-2

--- assignment2/program3/JUnitTest.java
+++ assignment2/program3/JUnitTest.java
@@ -1,14 +1,38 @@
 package com.ito.assignment2.program3;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.Test;
 
 public class JUnitTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test1() {
+		EvenOdd eo = new EvenOdd(1, 100);
+		Thread even = new Thread(new EvenThread(eo));
+		assertFalse(even.isAlive());
+	}
+	
+	@Test
+	public void test2() {
+		EvenOdd eo = new EvenOdd(1, 100);
+		Thread even = new Thread(new EvenThread(eo));
+		assertFalse(even.isInterrupted());
+	}
+	
+	@Test
+	public void test3() {
+		EvenOdd eo = new EvenOdd(1, 100);
+		Thread even = new Thread(new EvenThread(eo));
+		assertEquals("Thread-3",even.getName());
+	}
+	
+	@Test
+	public void test4() {
+		EvenOdd eo = new EvenOdd(1, 100);
+		Thread even = new Thread(new EvenThread(eo));
+		assertEquals("Thread[main,5,main]", even.currentThread().toString());
 	}
 
 }
